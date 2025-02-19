a65b15fa2069d334779a340e76f1608922f1284c
AllenIverson25 <526jfriedman@frhsd.com>
2025-02-19 00:41:04 +0000

    big commit. moved all of images into folder and started my timeline logic!!!

---
+++ script.js
@@ -0,0 +1,41 @@
+const trumanQuotes = [
+    "America was not built on fear. America was built on courage, on imagination and an unbeatable determination to do the job at hand.",
+    "The buck stops here.",
+    "It is amazing what you can accomplish if you do not care who gets the credit.",
+    "Men make history and not the other way around.",
+    "If you can't convince them, confuse them."
+  ];
+  
+  const timelineEvents = [
+    { 
+      year: 1884, 
+      event: "Born in Lamar, Missouri", 
+      details: "Harry S. Truman was born on May 8, 1884, to John Anderson Truman and Martha Ellen Young Truman.",
+      image: "imgs/birth.png"
+    },
+    { 
+      year: 1917, 
+      event: "Military Service in WWI", 
+      details: "Served with distinction in World War I as an artillery officer, leading Battery D of the 129th Field Artillery.",
+      image: "imgs/ww1serve.png"
+    },
+    { 
+      year: 1945, 
+      event: "Became 33rd President", 
+      details: "Assumed the presidency following FDR's death, facing the challenges of ending World War II and establishing postwar order.",
+      image: "imgs/president33.png"
+    },
+    { 
+      year: 1947, 
+      event: "Truman Doctrine", 
+      details: "Established the Truman Doctrine, pledging American support to democratic nations under threat from authoritarian forces.",
+      image: "imgs/trumandoctrine.png"
+    },
+    { 
+      year: 1948, 
+      event: "Marshall Plan", 
+      details: "Implemented the Marshall Plan, providing crucial economic aid to rebuild Western Europe after World War II.",
+      image: "imgs/Marshallplan.png"
+    }
+  ];
+  
